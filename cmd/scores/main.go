5f475abf81e35d594ff814c3672f681f5d4edbc1
Scott Clarke <sdclarke123@gmail.com>
2020-06-13 19:46:22 +0100

    Initialise empty scores arrays

--- cmd/scores/main.go
+++ cmd/scores/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net"
+	"os"
 
 	"github.com/sdclarke/scores/pkg/proto/scoreupdate"
 	"github.com/sdclarke/scores/pkg/scorer"
@@ -11,6 +14,25 @@ import (
 )
 
 func main() {
+	scores := struct {
+		Mean    []struct{} `json:"mean"`
+		Median  []struct{} `json:"median"`
+		Maximum []struct{} `json:"maximum"`
+	}{
+		Mean:    []struct{}{},
+		Median:  []struct{}{},
+		Maximum: []struct{}{},
+	}
+	scoresJsonString, err := json.MarshalIndent(scores, "", "\t")
+	if err != nil {
+		log.Fatalf("Failed to marshal scores: %#v", err)
+	}
+
+	scoresFile, err := os.OpenFile("interface/src/assets/scores.json", os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0664)
+	if err != nil {
+		log.Fatalf("Failed to open file scores.json: %#v", err)
+	}
+	fmt.Fprintf(scoresFile, string(scoresJsonString))
 	scorer := scorer.NewMaximumScorer(
 		scorer.NewMedianScorer(
 			scorer.NewMeanScorer()))
